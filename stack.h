b5e5a0710559f56adcd3378734f130f4fd99b108
Vyom Budha <164761373+Vyombudha@users.noreply.github.com>
2025-07-27 17:02:06 +0545

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,7 +1,6 @@
 #ifndef STACK_H
 #define STACK_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,64 +9,53 @@
 #define MAX_BUFFER 257
 #define NO_VALUE '\0'
 
-
 // enums based codes to handle stack proccess!
-typedef enum{
-    RESPONSE_INITIATED,
-    STACK_PROCESS_SUCCESSFUL,
-    STACK_PUSH_NODE_ALLOCATION_ERROR,
-    STACK_EMPTY
-}stackCodes;
+typedef enum
+{
+    RESPONSE_INITIATED = 0,
+    STACK_PROCESS_SUCCESSFUL = 1,
+    STACK_PUSH_NODE_ALLOCATION_ERROR = 2,
+    STACK_EMPTY = 3,
+    STACK_NULL_POINTER = 4
+} stackCodes;
 
-
-// messages for appropriate Stack proccess Code
-const char* STACK_MESSAGES[4] = {
-    "RESPONSE_INITIATED",
-    "STACK_PROCESS_SUCCESSFUL",
-    "STACK_PUSH_NODE_ALLOCATION_ERROR",
-    "STACK_EMPTY"
-};
-
-
+extern const char *STACK_MESSAGES[5];
 
 // A struct to handle Struct Prccess Codes  and Return Values!
-typedef struct{
-    stackCodes  code;
+typedef struct
+{
+    stackCodes code;
+    char returnValue;
     char message[MAX_BUFFER];
-    char poppedData;
-}stackResponse;
+} stackResponse;
 
-
-
-typedef struct Node{
+typedef struct Node
+{
     char data;
-    struct Node* nextNode;
-}Node;
+    struct Node *nextNode;
+} Node;
 
-
-typedef struct{
+typedef struct
+{
     size_t size;
-    Node* top;
-}Stack;
-
-
-
-
+    Node *top;
+} Stack;
 
 // Functions for Response struct!
 stackResponse newResponse(stackCodes code);
 
 // Functions for handling Stack Operations!
-Node* newNode(char data);
+Node *newNode(char data);
 Stack newStack(void);
-stackResponse push(Stack *stack,char data);
+stackResponse push(Stack *stack, char data);
 stackResponse pop(Stack *stack);
 stackResponse printStack(const Stack *stack);
 stackResponse destroyStack(Stack *stack);
+stackResponse peek(const Stack *stack);
 
 // Helper Functions for Stack!
+bool isNULL(const Stack *stack);
 bool isEmpty(const Stack *stack);
 size_t size(const Stack *stack);
-char peek(const Stack *stack);
 
 #endif // STACK_H
